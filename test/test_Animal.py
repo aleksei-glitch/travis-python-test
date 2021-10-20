354e51a175b0b7c7a45466d8a0a4f59848682b94
aleksei-glitch <76838739+aleksei-glitch@users.noreply.github.com>
2021-10-20 21:14:01 +0300

    Update test_Animal.py

--- test/test_Animal.py
+++ test/test_Animal.py
@@ -3,4 +3,6 @@ from Animal import Animal
 
 def test_duck():
   a1 = Animal('duck', 'donald', 'quack')
-  assert "The duck is named  \"donald\" and says \"quack\"." == a1.print_animal()
+  testStr = a1.print_animal()
+
+  assert testStr == "The duck is named  \"donald\" and says \"quack\"."
